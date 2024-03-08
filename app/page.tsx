1dfa37f62b116a5817f9caba235c64b34468d900
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-03-08 10:37:12 -0300

    feature: add header header skeleton and fix page height to flex

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
+import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen min-w-screen flex-col items-center justify-between bg-main-color">
-      <div className="flex w-full h-screen font-mono justify-center items-center">
+    <main className="flex flex-col w-screen h-screen bg-main-color">
+      <Header />
+      <div className="flex w-full flex-1 font-mono justify-center items-center">
         <article className="flex flex-col items-center">
-          <h1 className="flex font-main mb-4 text-4xl text-center font-bold leading-none tracking-tight text-second-color md:text-5xl lg:text-6xl">What if Internet Ends</h1>
+          <h2 className="flex font-main mb-4 text-4xl text-center font-bold leading-none tracking-tight text-second-color md:text-5xl lg:text-6xl">What if Internet Ends</h2>
           <p className="font-main font-bold">Site is under construction</p>
         </article>
       </div>
---
+++ components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="w-full h-14 bg-slate-700">
+            <h1>What if Internet Ends</h1>
+        </header>
+    )
+}
