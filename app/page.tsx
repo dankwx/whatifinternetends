3094b7f35863a6eb273f5b89ad8cad9b760dda81
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-03-09 02:28:28 -0300

    chore: start of home text

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,14 @@ export default function Home() {
   return (
     <main className="flex flex-col w-screen h-screen bg-main-color">
       <Header />
-      <div className="flex w-full flex-1 font-mono justify-center items-center">
-        <article className="flex flex-col items-center">
-          <h2 className="flex font-main mb-4 text-4xl text-center font-bold leading-none tracking-tight text-second-color md:text-5xl lg:text-6xl">What if Internet Ends</h2>
-          <p className="font-main font-bold">Site is under construction</p>
+      <div className="flex w-full flex-1 font-mono  items-center">
+        <article className="flex w-1/2 h-full flex-col">
+          <h2 className="font-bold font-main text-4xl mx-8 mt-16"
+          >Have you ever thought about what the use of computers would be if the internet went away?</h2>
+          <p className="font-main mx-8 mt-8 leading-6"
+          >Well, this is an unlikely scenario, but just as people like to stock up on groceries when there is news of wars and lockdowns, wouldn't it be nice to stock up on "digital groceries" on your computer?</p>
         </article>
+        <div className="flex w-1/2 h-full bg-slate-900"></div>
       </div>
     </main>
   );
