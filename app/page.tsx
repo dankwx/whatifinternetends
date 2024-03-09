aed9912cfae38024669db3f9a1ea4536595bb2c5
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-03-09 04:02:07 -0300

    chore: add img to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Header from "@/components/Header/Header";
+import cloudImg from "../public/cloud.png"
 
 export default function Home() {
   return (
@@ -12,7 +13,9 @@ export default function Home() {
           <p className="font-main mx-8 mt-8 leading-6"
           >Well, this is an unlikely scenario, but just as people like to stock up on groceries when there is news of wars and lockdowns, wouldn't it be nice to stock up on "digital groceries" on your computer?</p>
         </article>
-        <div className="flex w-1/2 h-full bg-slate-900"></div>
+        <div className="flex w-1/2 h-full">
+          <Image className="h-52 w-52" src={cloudImg}  alt="ff" />
+        </div>
       </div>
     </main>
   );
