45067706bc3e628de740721ecc6e7823902da022
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-03-09 04:05:49 -0300

    chore: add hr tag to home page

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,12 @@ export default function Home() {
         <article className="flex w-1/2 h-full flex-col">
           <h2 className="font-bold font-main text-4xl mx-8 mt-16"
           >Have you ever thought about what the use of computers would be if the internet went away?</h2>
-          <p className="font-main mx-8 mt-8 leading-6"
+          <hr className="border border-solid border-slate-950 mx-8 my-2" />
+          <p className="font-main mx-8 mt-4 leading-6"
           >Well, this is an unlikely scenario, but just as people like to stock up on groceries when there is news of wars and lockdowns, wouldn't it be nice to stock up on "digital groceries" on your computer?</p>
         </article>
         <div className="flex w-1/2 h-full">
-          <Image className="h-52 w-52" src={cloudImg}  alt="ff" />
+          <Image className="h-52 w-52" src={cloudImg} alt="ff" />
         </div>
       </div>
     </main>
