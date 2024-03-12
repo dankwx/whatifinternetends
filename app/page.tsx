cdf98cf6e4f6945102293e22405bacc723b79429
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-03-12 02:05:24 -0300

    chore: change header s tyle

--- app/page.tsx
+++ app/page.tsx
@@ -7,20 +7,19 @@ export default function Home () {
     <main className='flex flex-col w-screen h-screen items-center bg-main-color'>
       <Header />
       <Navbar />
-      <div className='flex w-full flex-1 font-mono  items-center'>
-        <article className='flex w-1/2 h-full flex-col'>
-          <h2 className='font-bold font-main text-4xl mx-8 mt-16'>
-            Have you ever thought about what the use of computers would be if
-            the internet went away?
+      <div className='flex w-full px-80 flex-1 font-mono items-center'>
+        <article className='flex w-full h-full flex-col'>
+          <h2 className='font-bold font-main text-2xl mt-16'>
+            In case the internet goes out, anticipating your downloads can save
+            you money and time
           </h2>
-          <hr className='border border-solid border-slate-950 mx-8 my-2' />
-          <p className='font-main mx-8 mt-4 leading-6'>
+          <hr className='border border-solid border-slate-950' />
+          <p className='font-main mt-4 leading-6'>
             Well, this is an unlikely scenario, but just as people like to stock
             up on groceries when there is news of wars and lockdowns, wouldn't
             it be nice to stock up on "digital groceries" on your computer?
           </p>
         </article>
-        <div className='flex w-1/2 h-full'></div>
       </div>
     </main>
   )
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,9 @@
+import Image from 'next/image'
+import cat from './cat.png'
 export default function Header () {
   return (
-    <header className='flex items-center w-full h-14 bg-terciary-color border-solid border-b'>
+    <header className='flex items-center justify-center w-full h-32 border-solid border-b'>
+      <Image src={cat} width={65} alt='cat' />
       <h1 className='text-second-color font-main font-bold text-2xl ml-4'>
         What if Internet Ends
       </h1>
